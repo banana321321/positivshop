02c7ae7a8b4ce10a51064eba19a3d0ed2c9e36e4
banana321321 <daneel.121.b@gmail.com>
2024-06-12 14:26:35 +0500

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -67,10 +67,17 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join('/tmp', 'db.sqlite3'),
     }
 }
 
