c391ca86e01c5f7bd9a5dc145c35ca3b8c4cf3c6
banana321321 <daneel.121.b@gmail.com>
2024-06-12 14:29:47 +0500

    Update settings.py

--- ecom/settings.py
+++ ecom/settings.py
@@ -67,19 +67,19 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join('/tmp', 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join('/tmp', 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
